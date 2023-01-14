f12bf6c76e4ee626da16bd38f39921945c1cb771
Dion <dionang11@gmail.com>
2023-01-14 18:55:03 +0800

    chapter 4

--- nodejs-youtube/server.js
+++ nodejs-youtube/server.js
@@ -1,65 +1,7 @@
-const fsPromises = require("fs").promises;
-const path = require("path");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
 
-const fileOps = async () => {
-  try {
-    const data = await fsPromises.readFile(
-      path.join(__dirname, "files", "starter.txt"),
-      "utf8",
-      (err, data) => {
-        if (err) throw err;
-        console.log(data);
-      }
-    );
-  } catch (err) {
-    console.log(err);
-  }
-};
+console.log("test");
+console.log(format(new Date(), "yyyMMdd\tHH:mm:ss"));
 
-// read from file
-fs.readFile(
-  path.join(__dirname, "files", "starter.txt"),
-  "utf8",
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }
-);
-
-// write into file
-fs.writeFile(
-  path.join(__dirname, "files", "reply.txt"),
-  "Nice to meet you",
-  (err) => {
-    if (err) throw err;
-    console.log("Write complete");
-
-    // edit file
-    // will create file if doesnt exist
-    fs.appendFile(
-      path.join(__dirname, "files", "reply.txt"),
-      "Append text",
-      (err) => {
-        if (err) throw err;
-        console.log("Append complete");
-      }
-    );
-  }
-);
-
-// edit file
-// will create file if doesnt exist
-fs.appendFile(
-  path.join(__dirname, "files", "reply.txt"),
-  "Append text",
-  (err) => {
-    if (err) throw err;
-    console.log("Append complete");
-  }
-);
-
-// exit on uncaught error
-process.on("uncaughtException", (err) => {
-  console.error(`There was an uncaught error: ${err}`);
-  process.exit(1);
-});
+console.log(uuid());
