b09512acab1800fbb5fd4c3a40ae5bf1e8383647
Dion <dionang11@gmail.com>
2023-01-14 15:26:25 +0800

    nodejs youtube

---
+++ nodejs-youtube/test.js
@@ -0,0 +1,13 @@
+console.log("consolelog 1");
+
+function test1() {
+  console.log("consolelog 2");
+}
+test1();
+
+function test2() {
+  console.log("consolelog 3");
+}
+test2();
+
+console.log("consolelog 4");
